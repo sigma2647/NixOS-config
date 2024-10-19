e4ac2b5f31edf4451eeca81eec9c57aa1f6aaad5
sigma2647 <1169446204@qq.com>
2024-10-19 19:06:42 +0800

    udpa

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     substituters = [
       # "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
       # "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/nixpkgs-unstable"
+      # "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/nixpkgs-unstable"
       # "https://mirrors.ustc.edu.cn/nix-channels/store"
       "https://cache.nixos.org/"
     ];
