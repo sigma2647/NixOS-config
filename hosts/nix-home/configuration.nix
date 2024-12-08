d8dbfe0a7ce585bd885dfc0bbfcc2ed6f6126c14
sigma2647 <1169446204@qq.com>
2024-12-09 00:56:54 +0800

    dup

--- hosts/nix-home/configuration.nix
+++ hosts/nix-home/configuration.nix
@@ -139,7 +139,7 @@
     gcc
     libffi
     openssl
-    # zathura
+    zathura
     lan-mouse
 
 
@@ -188,6 +188,16 @@
   # sound
   # sound.enable = true;
   # hardware.pulseaudio.enable = true;
+  #
+  #
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
 
   nix = {
     settings = {
