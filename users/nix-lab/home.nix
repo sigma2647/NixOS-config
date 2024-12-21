fc7acd238343f85f72069f6db38f1219c9cfc7f1
sigma2647 <1169446204@qq.com>
2024-12-22 02:47:35 +0800

    update

---
+++ users/nix-lab/home.nix
@@ -0,0 +1,37 @@
+{ pkgs, ... }: {
+# { pkgs, hostnames, ... }: {
+  home.stateVersion = "24.11"; # 请根据实际版本设置
+  imports = [
+    # ../../home/core.nix
+    # ../../home/shell
+    # ../../home/lf
+    # ../../home/yazi/yazi.nix
+    # ../../home/kitty
+    # ../../home/pyprland
+    # ../../home/waybar
+    # ../../home/rofi
+    # ../../home/fcitx5
+    # ../../home/hyprland/hyprland.nix
+
+  ];
+
+  home.packages = with pkgs; [
+    tealdeer
+    bat
+    # tree
+    btop
+    cmatrix
+    pipx
+    uv
+  ];
+  programs.git = {
+    enable = true;
+    userName = "sigma2647";
+    userEmail = "1169446204@qq.com";
+  };
+
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
+}
