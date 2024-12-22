d0727d52f9461bbb0ea5ddaba000b5da354e9a30
sigma2647 <1169446204@qq.com>
2024-12-22 20:10:21 +0800

    up

--- home/bash/default.nix
+++ home/bash/default.nix
@@ -4,27 +4,39 @@
   programs.bash = {
     enable = true;
 
-    shellAliases = {
-      dot = "cd ~/dotfile";
-      oo = "cd ~/note/sigma";
-      mkdir = "mkdir -p";
-      g = "lazygit";
-
-      myip = "curl ifconfig.me; echo";
-    };
 
     initExtra = ''
-      HISTSIZE = 10000
+      export HISTSIZE = 10000
       stty -ixon
-      HISTTIMEFORMAT = "%F %T"
+      export HISTTIMEFORMAT = "%F %T"
+      export MANPAGER = "nvim +Man!"
 
 
       f() {
         find . -iname "*$1*"
       }
 
+      function y() {
+      	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+      	yazi "$@" --cwd-file="$tmp"
+      	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+      		builtin cd -- "$cwd"
+      	fi
+      	rm -f -- "$tmp"
+      }
     '';
     
+    shellAliases = {
+      dot = "cd ~/dotfile";
+      oo = "cd ~/note/sigma";
+      mkdir = "mkdir -p";
+      g = "lazygit";
+      v = "nvim";
+      t = "tmux";
+      l = "y";
+
+      myip = "curl ifconfig.me; echo";
+    };
     sessionVariables = {
       EDITOR = "nvim";
     };
