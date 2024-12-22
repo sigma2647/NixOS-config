83740543e1c030ef612ac60b88d0bd6ecef387b9
sigma2647 <1169446204@qq.com>
2024-12-22 23:37:56 +0800

    up

--- hosts/nix-lab/configuration.nix
+++ hosts/nix-lab/configuration.nix
@@ -4,6 +4,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ../../modules/services/tailscale.nix
+      ../../modules/network/host.nix
     ];
 
   # 使用 systemd-boot
