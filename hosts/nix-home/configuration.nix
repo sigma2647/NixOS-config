f0676fda5d6f47797712e2ba7c0b723aaa156751
sigma2647 <1169446204@qq.com>
2025-01-10 23:34:51 +0800

    up

--- hosts/nix-home/configuration.nix
+++ hosts/nix-home/configuration.nix
@@ -10,8 +10,9 @@ pkgs,
 {
   imports =
     [
-      ./hardware-configuration.nix
       ../fonts.nix
+      ./packages.nix
+      ./hardware-configuration.nix
       ../../modules/services/tailscale.nix
       ../../modules/programs/kde-connect.nix
       ../../modules/system/time.nix
@@ -124,87 +125,6 @@ pkgs,
   # hardware.nvidia.enable = true;
 
   # 系统环境变量
-  environment.systemPackages = with pkgs; [
-    vim
-    neovim
-    git
-    wget
-    curl
-    vscode
-    tree
-
-    tmux
-    kitty
-    lf
-    lazygit
-
-    fastfetch
-
-    #(texlive.combine { inherit (texlive) scheme-full; })
-    zsh
-    starship
-    zoxide
-    pyenv
-    direnv
-    fzf
-
-    ripgrep
-
-    bat
-    pstree
-    nodejs
-
-    nautilus
-    xfce.thunar
-    # ---
-    xfce.thunar-archive-plugin
-    xfce.thunar-volman
-    xfce.tumbler # 缩略图支持
-    papirus-icon-theme
-    adwaita-qt
-    # ---
-
-
-
-
-    python3
-    # zed
-    # unstable.pywal16
-    #
-    gcc
-    libffi
-    openssl
-
-
-    stow
-    flatpak
-
-    # zed-editor
-
-    firefox
-
-    # hyprland                   # 安装 Hyprland
-    waybar                     # 可选: Wayland 状态栏
-    rofi                       # 可选: 应用启动器
-    #dunst                      # 可选: 通知管理器
-
-
-
-
-    networkmanager
-    networkmanagerapplet
-    openssh
-    home-manager
-
-    #gtk2
-    #gtk3
-    #gtk4
-
-    emacs29-pgtk
-
-    fcitx5-rime
-    rime-data
-  ];
 
   environment.variables.EDITOR = "nvim";
 
---
+++ hosts/nix-home/packages.nix
@@ -0,0 +1,85 @@
+{ pkgs, ... }: {
+
+  environment.systemPackages = with pkgs; [
+    vim
+    neovim
+    git
+    wget
+    curl
+    vscode
+    tree
+
+    tmux
+    kitty
+    lf
+    lazygit
+
+    fastfetch
+
+    #(texlive.combine { inherit (texlive) scheme-full; })
+    zsh
+    starship
+    zoxide
+    pyenv
+    direnv
+    fzf
+
+    ripgrep
+
+    bat
+    pstree
+    nodejs
+
+    nautilus
+    xfce.thunar
+    # ---
+    xfce.thunar-archive-plugin
+    xfce.thunar-volman
+    xfce.tumbler # 缩略图支持
+    papirus-icon-theme
+    adwaita-qt
+    # ---
+
+
+
+
+    python3
+    # zed
+    # unstable.pywal16
+    #
+    gcc
+    libffi
+    openssl
+
+
+    stow
+    flatpak
+
+    # zed-editor
+
+    firefox
+
+    # hyprland                   # 安装 Hyprland
+    waybar                     # 可选: Wayland 状态栏
+    rofi                       # 可选: 应用启动器
+    #dunst                      # 可选: 通知管理器
+
+
+
+
+    networkmanager
+    networkmanagerapplet
+    openssh
+    home-manager
+
+    #gtk2
+    #gtk3
+    #gtk4
+
+    emacs29-pgtk
+
+    fcitx5-rime
+    rime-data
+    qemu
+  ];
+}
