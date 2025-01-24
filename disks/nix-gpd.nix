f9cea7e90cb2a5b3e2b94359f0d64b91cd8ae248
lawrence <61632637+sigma2647@users.noreply.github.com>
2025-01-25 02:59:26 +0800

    创建 nix-gpd.nix

---
+++ disks/nix-gpd.nix
@@ -0,0 +1,57 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        device = "/dev/nvme0n0";  # 替换为你的磁盘设备
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            # EFI 引导分区
+            ESP = {
+              type = "EF00";  # EFI 分区类型
+              size = "1G";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+
+            # Swap 分区
+            swap = {
+              size = "32G";
+              content = {
+                type = "swap";
+              };
+            };
+
+            # Btrfs 根分区
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "btrfs";
+                mountpoint = "/";
+                mountOptions = [ "compress=zstd" "ssd" "noatime" ];  # 启用压缩和 SSD 优化
+
+                # Btrfs 子卷配置
+                btrfs.subvolumes = {
+                  "@nix" = {
+                    mountpoint = "/nix";
+                  };
+                  "@home" = {
+                    mountpoint = "/home";
+                  };
+                  "@root" = {
+                    mountpoint = "/";
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
