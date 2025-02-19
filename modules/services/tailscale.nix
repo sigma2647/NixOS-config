b83c0d28bdf0d6a2612f9cedb768c12e6625ec85
sigma2647 <1169446204@qq.com>
2025-02-19 19:29:04 +0800

    upda

--- modules/services/tailscale.nix
+++ modules/services/tailscale.nix
@@ -1,10 +1,23 @@
-{config, lib, pkgs, ...}:
-
+{config, lib, pkgs, pkgs-unstable,...}:
 {
+  # 添加内核模块支持
+  boot.kernelModules = [
+    "iptable_filter"
+    "iptable_nat"
+    "nf_nat"
+    "nf_conntrack"
+    "nf_conntrack_netlink"
+    "xt_mark"
+  ];
+
+  # 启用 nftables
+  networking.nftables.enable = true;
+
+  # 原有的 tailscale 配置
   services.tailscale = {
-	  enable = true;
-	  package = pkgs.tailscale;
-	  useRoutingFeatures = "both";
+    enable = true;
+    package = pkgs-unstable.tailscale;
+    useRoutingFeatures = "both";
   };
 
   networking.firewall = {
