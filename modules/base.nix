9859cb2f019b272fb1e7eeb3566811ab0dc084b5
sigma2647 <1169446204@qq.com>
2025-02-22 19:08:35 +0800

    up

--- modules/base.nix
+++ modules/base.nix
@@ -6,6 +6,18 @@
     experimental-features = [ "nix-command" "flakes" ];
     max-jobs = "auto";
     use-case-hack = true;
+    substituters = [
+      "https://mirrors.cernet.edu.cn/nix-channels/store"  # 中科大
+      "https://mirrors.ustc.edu.cn/nix-channels/store"  # 中科大
+      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"  # 清华
+      "https://mirror.sjtu.edu.cn/nix-channels/store"  # 上海交大 
+      "https://mirrors.bfsu.edu.cn/nix-channels/store"  # 北外
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
   
   # 支持非 NixOS 编译的程序
