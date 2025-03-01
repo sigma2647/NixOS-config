835e3baf003cfbf6e98fd5ecc16e9df5f1a63f4a
sigma2647 <1169446204@qq.com>
2025-03-02 03:04:56 +0800

    up

--- modules/network/host.nix
+++ modules/network/host.nix
@@ -2,11 +2,11 @@
 
 {
   networking.extraHosts = ''
-    100.74.252.119  nix-lab
-    100.119.224.41  mini
-    100.92.60.84    desktop-2ij6cg8
-    100.101.26.120  iphone-15-pro-max
-    100.125.108.10  mbp
-    100.92.84.62    nix-home
+    # 100.74.252.119  nix-lab
+    # 100.119.224.41  mini
+    # 100.92.60.84    desktop-2ij6cg8
+    # 100.101.26.120  iphone-15-pro-max
+    # 100.125.108.10  mbp
+    # 100.92.84.62    nix-home
   '';
 }
