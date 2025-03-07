4dcd01ac451fdadadf78f90120e2b4157810e057
sigma2647 <1169446204@qq.com>
2025-03-08 00:19:32 +0800

    dup

--- home/hyprland/hyprland.nix
+++ home/hyprland/hyprland.nix
@@ -11,21 +11,34 @@
       # "$fileManager" = "$terminal -e sh -c 'yazi'";
       "$fileManager" = "thunar";
       "$mainMod" = "SUPER";
+      "$menu" = "wofi --show drun";
 
       monitor = ",preferred,auto,auto";
 
+
       exec-once = "waybar";
       exec-once = "pypr";
+      exec-once = "swww-daemon";
+      # exec-once = "mako";
+
 
       bind = "$mainMod, h, movefocus, l";
       bind = "$mainMod, l, movefocus, r";
       bind = "$mainMod, k, movefocus, u";
       bind = "$mainMod, j, movefocus, d";
 
-      env = 
+      bind = "alt,backslash,exec,pypr toggle term && hyprctl dispatch bringactivetotop";
+      bind = "$mainMod, Space, exec, pkill rofi || rofi -show drun";
 
-      env = XCURSOR_SIZE,24
-      env = HYPRCURSOR_SIZE,24
+
+      # env = 
+
+      env = XCURSOR_SIZE,24;
+      env = HYPRCURSOR_SIZE,24;
+
+
+      windowrule = "pseudo, fcitx";
+      
 
 
     }
