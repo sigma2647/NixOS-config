ca6e62bf38dcc462d30b47db74c854cb74c0badc
sigma2647 <1169446204@qq.com>
2025-03-13 20:18:00 +0800

    update

--- modules/docker.nix
+++ modules/docker.nix
@@ -1,17 +1,24 @@
 { pkgs, ... }:
 {
-  # Enables docker in rootless mode
   virtualisation = {
-    docker.enable = true;
-    docker.rootless = {
+    docker = {
       enable = true;
-      setSocketVariable = true;
+      rootless = {
+        enable = true;
+        setSocketVariable = true;
+      };
+      daemon.settings = {
+        data-root = "/var/lib/docker";
+      };
     };
-    # Enables virtualization for virt-manager
     libvirtd.enable = true;
   };
+  
   environment.systemPackages = with pkgs; [
     docker-compose
+    docker-client
   ];
-
+  
+  # 确保相关用户服务也启用
+  systemd.user.services.docker.wantedBy = [ "default.target" ];
 }
