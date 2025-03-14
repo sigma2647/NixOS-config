29db88aeaeac841e2d5b182953a7153f735ae90d
lawrence <61632637+sigma2647@users.noreply.github.com>
2025-03-14 22:35:11 +0800

    更新 docker.nix

--- modules/docker.nix
+++ modules/docker.nix
@@ -1,33 +1,34 @@
 { pkgs, ... }:
-
 {
-  # Docker Rootless 模式核心配置
-  virtualisation.docker = {
-    enable = true;
-    rootless = {
+  virtualisation = {
+    docker = {
       enable = true;
-      setSocketVariable = true;  # 设置 DOCKER_HOST 环境变量
+      rootless = {
+        enable = true;
+        setSocketVariable = true;
+      };
+      daemon.settings = {
+        data-root = "/var/lib/docker";
+        registry-mirrors = [ 
+          "https://docker.m.daocloud.io"
+          "https://docker.imgdb.de"
+          "https://docker-0.unsee.tech"
+          "https://docker.hlmirror.com"
+          "https://docker.1ms.run"
+          "https://func.ink" 
+       ];
+      };
     };
-
-    # 关键：使用官方文档指定的 daemon.settings
-    daemon.settings = {
-      registry-mirrors = [ 
-        "https://docker.m.daocloud.io"
-        "docker.imgdb.de"
-        "https://docker-0.unsee.tech"
-        "https://docker.hlmirror.com"
-        "https://docker.1ms.run"
-        "https://func.ink" 
-      ];
-    };
+    libvirtd.enable = true;
   };
+  
+  environment.systemPackages = with pkgs; [
+    docker-compose
+    docker-client
+  ];
 
-  # 必须的用户命名空间配置
-  users.users.lawrence = {
-    isNormalUser = true;
-    extraGroups = [ "docker" ];
-    subUidRanges = [{ startUid = 100000; count = 65536; }];
-    subGidRanges = [{ startGid = 100000; count = 65536; }];
-  };
 
+  
+  # 确保相关用户服务也启用
+  systemd.user.services.docker.wantedBy = [ "default.target" ];
 }
