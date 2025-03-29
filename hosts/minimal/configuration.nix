e505c8f603b445025cb389d1b57279568155d910
sigma2647 <1169446204@qq.com>
2025-03-29 23:02:29 +0800

    upda

--- hosts/minimal/configuration.nix
+++ hosts/minimal/configuration.nix
@@ -49,7 +49,7 @@
   users.users.lawrence = {                   # 将 'xing' 替换为你的用户名
     isNormalUser = true;
     createHome = true;
-		extraGroups = [ "wheel" "networkmanager" ]; # 允许用户使用 sudo
+    extraGroups = [ "wheel" "networkmanager" ]; # 允许用户使用 sudo
     packages = with pkgs; [              # 默认安装的用户包
       vim
       wget
