9d61447b2bdefdc76baa5dc070d93353b15b32fe
sigma2647 <1169446204@qq.com>
2025-04-01 14:43:59 +0800

    up

--- home/rofi/default.nix
+++ home/rofi/default.nix
@@ -13,7 +13,7 @@
     "rofi/config.rasi" = {
       text = ''
         * {
-            font: "JetbrainsMono Nerd Font 13";
+            font: "Maple Mono NL NF CN 15";
             g-spacing: 10px;
             g-margin: 0;
             b-color: #000000FF;
