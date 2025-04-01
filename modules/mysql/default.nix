a640796c14da10d7fb18a5a2b1005aa5e1012dc6
sigma2647 <1169446204@qq.com>
2025-04-01 18:58:55 +0800

    up

---
+++ modules/mysql/default.nix
@@ -0,0 +1,17 @@
+{
+  config,
+  ipkgs,
+  ...
+}: {
+  containers.mysql = {
+    autoStart = false;
+    config = {
+      services.mysql.enable = true;
+      services.mysql.package = nixpkgs.mysql;
+      services.mysql.user = "root";
+      services.mysql.initialScript = ./init.sql;
+    };
+    ephemeral = true;
+  };
+}
+
