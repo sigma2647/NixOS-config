35cf5db460924f37f256130161c9afd2886db6aa
sigma2647 <1169446204@qq.com>
2025-04-02 22:14:55 +0800

    c

--- modules/base.nix
+++ modules/base.nix
@@ -21,7 +21,7 @@
       auto-optimise-store = true;
       substituters = [
         "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-        "https://mirror.sjtu.edu.cn/nix-channels/store"
+        # "https://mirror.sjtu.edu.cn/nix-channels/store"
         "https://mirrors.ustc.edu.cn/nix-channels/store"
         "https://cache.nixos.org"
 
