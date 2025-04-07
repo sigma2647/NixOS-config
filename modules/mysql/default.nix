1af09883bf295ece392263090b9f2b5e3a6c7636
sigma2647 <1169446204@qq.com>
2025-04-07 19:16:26 +0800

    up

--- modules/mysql/default.nix
+++ modules/mysql/default.nix
@@ -10,8 +10,18 @@
       services.mysql.package = pkgs.mariadb;
       services.mysql.user = "root";
       services.mysql.initialScript = ./init.sql;
+      services.mysql.settings = {
+        mysqld = {
+          bind-address = "0.0.0.0";  # 监听所有网络接口
+        };
+      };
     };
     ephemeral = true;
+    bindMounts = {
+      "/var/lib/mysql" = {
+        # hostPath = "/path/to/mysql/data";  # 可选：如果你希望数据持久化
+        isReadOnly = false;
+      };
+    };
   };
 }
-
