dbe928cbf8d43f6121082bb623cd5c8205fb5916
sigma2647 <1169446204@qq.com>
2025-04-21 22:48:45 +0800

    upa

--- sh/add-channel.sh
+++ sh/add-channel.sh
@@ -1,8 +1,10 @@
 # nix-channel --add https://mirrors.ustc.edu.cn/nix-channels/nixos-23.11 nixos        # 请注意系统版本
 # nix-channel --add https://mirrors.ustc.edu.cn/nix-channels/nixos-24.05 nixos        # 请注意系统版本
+# nix-channel --add https://mirrors.ustc.edu.cn/nix-channels/nixos-24.11 pkgs        # 请注意系统版本
+# nix-channel --add https://mirrors.ustc.edu.cn/nix-channels/nixpkgs-unstable pkgs-unstable # 订阅镜像仓库频道
+
 # nix-channel --add https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store
-nix-channel --add https://mirrors.ustc.edu.cn/nix-channels/nixos-24.11 pkgs        # 请注意系统版本
-nix-channel --add https://mirrors.ustc.edu.cn/nix-channels/nixpkgs-unstable pkgs-unstable # 订阅镜像仓库频道
+nix-channel --add https://nixos.org/channels/nixos-unstable nixpkgs-unstable
 
 nix-channel --list                                                                  # 列出频道
 nix-channel --update                                                                # 更新并解包频道
