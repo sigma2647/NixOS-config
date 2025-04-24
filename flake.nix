4a271feb1ed386a97ebb2ea815c0ab3bc51a2e0d
sigma2647 <1169446204@qq.com>
2025-04-25 02:14:19 +0800

    up

--- flake.nix
+++ flake.nix
@@ -28,70 +28,69 @@
   };
 
   outputs = { self, home-manager, nixpkgs, nixpkgs-unstable, ... } @ inputs:
-    let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-      forAllSystems = nixpkgs-unstable.lib.genAttrs supportedSystems;
-      
-      pkgsBySystem = forAllSystems (system: {
-        stable = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-        unstable = import nixpkgs-unstable {
-          inherit system;
-          config.allowUnfree = true;
-        };
-      });
-
-
-     lib = nixpkgs.lib;
-
-     mkSystem = { hostname, username, system ? "x86_64-linux", extraModules ? [] }: 
-      let
-        specialArgs = {
-          inherit username hostname inputs;
-          pkgs = pkgsBySystem.${system}.stable;
-          pkgs-unstable = pkgsBySystem.${system}.unstable;
-        };
-      in
-
-      assert lib.assertMsg (builtins.elem system supportedSystems) "Unsupported system: ${system}";
-
-      nixpkgs.lib.nixosSystem {
+  let
+    supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+    forAllSystems = nixpkgs-unstable.lib.genAttrs supportedSystems;
+    
+    pkgsBySystem = forAllSystems (system: {
+      stable = import nixpkgs {
         inherit system;
-        inherit specialArgs;
-        modules = [
-          ./hosts/${hostname}/configuration.nix
-          ./modules/base.nix
-          home-manager.nixosModules.home-manager {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              backupFileExtension = "backup";
-              extraSpecialArgs = inputs // specialArgs;
-              users.${username} = import ./users/${hostname}/home.nix;
-            };
-          }
-        ] ++ extraModules;
+        config.allowUnfree = true;
       };
-
-      # Helper function for creating home-manager configurations
-      mkHomeConfig = { system, configFile, extraModules ? [] }: home-manager.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-        modules = [
-          configFile
-        ] ++ extraModules;
-        extraSpecialArgs = {
-          inherit inputs;
-          pkgs = pkgsBySystem.${system}.stable;
-          pkgs-unstable = pkgsBySystem.${system}.unstable;
-        };
+      unstable = import nixpkgs-unstable {
+        inherit system;
+        config.allowUnfree = true;
       };
+    });
 
+
+    lib = nixpkgs.lib;
+
+    mkSystem = { hostname, username, system ? "x86_64-linux", extraModules ? [] }: 
+    let
+      specialArgs = {
+        inherit username hostname inputs;
+        pkgs = pkgsBySystem.${system}.stable;
+        pkgs-unstable = pkgsBySystem.${system}.unstable;
+      };
     in
+
+    assert lib.assertMsg (builtins.elem system supportedSystems) "Unsupported system: ${system}";
+
+    nixpkgs.lib.nixosSystem {
+      inherit system;
+      inherit specialArgs;
+      modules = [
+        ./hosts/${hostname}/configuration.nix
+        ./modules/base.nix
+        home-manager.nixosModules.home-manager {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            backupFileExtension = "backup";
+            extraSpecialArgs = inputs // specialArgs;
+            users.${username} = import ./users/${hostname}/home.nix;
+          };
+        }
+      ] ++ extraModules;
+    };
+
+    # Helper function for creating home-manager configurations
+    mkHomeConfig = { system, configFile, extraModules ? [] }: home-manager.lib.homeManagerConfiguration {
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      modules = [
+        configFile
+      ] ++ extraModules;
+      extraSpecialArgs = {
+        inherit inputs;
+        pkgs = pkgsBySystem.${system}.stable;
+        pkgs-unstable = pkgsBySystem.${system}.unstable;
+      };
+    };
+  in
   {
 
     # 配置 NixOS
